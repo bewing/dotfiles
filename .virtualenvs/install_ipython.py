014d955e9358c9fee9083d4fb3a65137f9f077c6
Brandon Ewing <brandon.ewing@warningg.com>
2020-12-30 12:36:13 -0600

    Use pyenv-default-packages instead of postmkvirtual

--- .virtualenvs/install_ipython.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-if sys.version_info.major == 3:
-    ipy = "ipython"
-else:
-    ipy = "ipython<6"
-
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', ipy, 'pygments_style_solarized'])
